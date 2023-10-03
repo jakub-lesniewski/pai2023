90a9254245d54d87539f60c3dffbbce8f88c4a0d
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2023-10-03 19:34:02 +0200

    Frontend (uproszczony) + proxy do serwera na 8000

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>App</h1>
+  <TestComponent/>
+  <hr/>
+  <div class="footer">Footer</div>
+</template>
+
+<script>
+import TestComponent from './components/TestComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+}
+.footer {
+  font-size: small;
+  text-align: center;
+}
+</style>
--- server.js
+++ server.js
@@ -16,17 +16,25 @@ app.use(bodyParser.json())
 
 app.use(express.static(config.frontend))
 
-let test = 1
+let test = 0
 
+// pobieranie danych
 app.get('/test', (req, res) => {
     res.json({ test })
 })
 
+// zmiana danych
 app.post('/test', (req, res) => {
     test = req.body.test
     res.json({ test })
 })
 
+// zerowanie danych
+app.post('/delete', (req, res) => {
+    test = 0
+    res.json({ test })
+})
+
 app.listen(config.port, () => {
-    console.log('Backend listening on ', config.port)
+    console.log('Backend listening on port', config.port)
 })
