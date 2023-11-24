98f8dc55912d0a82e4f092c46f830e3498703dd4
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2023-11-24 10:20:10 +0100

    auth backend, Login Logout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,25 +11,85 @@
 
       <v-divider></v-divider>
 
+      <v-list v-if="user.username">
+        <v-list-item
+          :prepend-avatar="require('./assets/logo.png')"
+          :title="user.username"
+          :subtitle="user.email"
+        >
+        </v-list-item>
+      </v-list>
+
+      <v-list density="compact" nav>
+        <v-list-item key="Login" @click="loginDialog = true" prepend-icon="mdi-login" title="Login" exact v-if="!user.username"/>
+        <v-list-item key="Logout" @click="logoutConfirmation = true" prepend-icon="mdi-logout" title="Logout" exact v-if="user.username"/>
+      </v-list>
+
     </v-navigation-drawer>
 
     <v-main>
       <router-view></router-view>
     </v-main>
 
+    <v-dialog v-model="loginDialog" width="25em">
+      <Login @cancel="loginDialog = false" @loginFailed="loginDialog = false; loginFailed = true" @loginSuccess="onSuccessfulLogin"/>
+    </v-dialog>
+
+    <v-dialog v-model="logoutConfirmation" width="auto">
+      <ConfirmationDialog :question="'Are you sure to logout, ' + user.username + ' ?'" @ok="onLogout" @cancel="logoutConfirmation = false"/>
+    </v-dialog>
+
+    <v-snackbar v-model="loginSuccess" color="success" timeout="3000">Welcome on board, {{ user.username }}</v-snackbar>
+    <v-snackbar v-model="loginFailed" color="error" timeout="3000">Login failed</v-snackbar>
+
   </v-app>
 </template>
 
 <script>
+import Login from './components/Login.vue'
+import ConfirmationDialog from './components/ConfirmationDialog.vue'
+
 export default {
   name: 'App',
+  components: { Login, ConfirmationDialog },
+  methods: {
+    onSuccessfulLogin(data) {
+      this.user = data
+      this.loginDialog = false
+      this.loginSuccess = true
+    },
+    onLogout() {
+      this.logoutConfirmation = false
+      fetch('/auth', { method: 'DELETE' })
+      .then(res => {
+        res.json()
+        .then(body => this.user = body)
+        .catch(err => console.error(err.message))
+      })
+      .catch(err => console.error(err.message))
+    }
+  },
   data() {
     return {
       navigation: [
           { title: 'Dashboard', icon: 'mdi-view-dashboard', href: '#/' },
           { title: 'Persons', icon: 'mdi-account-multiple', href: '#/persons' }
-      ]
+      ],
+      loginDialog: false,
+      loginFailed: false,
+      loginSuccess: false,
+      logoutConfirmation: false,
+      user: {}
     }
+  },
+  mounted() {
+    fetch('/auth', { method: 'GET' })
+    .then(res => {
+      res.json()
+      .then(body => this.user = body)
+      .catch(err => console.error(err.message))
+    })
+    .catch(err => console.error(err.message))
   }
 }
 </script>
