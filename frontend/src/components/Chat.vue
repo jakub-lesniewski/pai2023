11db4dcd731e46bd179225f37948b7ca84060b07
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2023-12-12 13:45:57 +0100

    Czat we frontendzie

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -2,7 +2,16 @@
     <v-card variant="text">
         <v-card-title>Chat</v-card-title>
         <v-card-text>
+            <v-list>
+                <v-list-item v-for="(event, index) in posts" :key="index" :subtitle="event.username" :class="getItemClass(event)">
+                {{ event.message }}
+                </v-list-item>
+            </v-list>
         </v-card-text>
+        <v-card-actions>
+            <v-text-field variant="solo" label="Message" v-model="message"></v-text-field>
+            <v-btn variant="elevated" color="success" @click="send">Send</v-btn>
+        </v-card-actions>
     </v-card>
 </template>
   
@@ -12,21 +21,47 @@ export default {
     props: [ 'user' ],
     data() {
         return {
-            connection: null
+            connection: null,
+            message: '',
+            posts: []
+        }
+    },
+    methods: {
+        send() {
+            this.connection.send(JSON.stringify({
+                event: 'POST',
+                username: this.user.username || 'not-logged-in',
+                message: this.message
+            }))    
+        },
+        getItemClass(event) {
+            return event.username == this.user.username ? 'toRight' : 'toLeft'
         }
     },
     mounted() {
         this.connection = new WebSocket('ws://' + window.location.host + '/websocket')
         this.connection.onopen = () => {
             console.log('Websocket connection established')
-            this.connection.send('Now is ' + new Date())
+            this.connection.send(JSON.stringify({
+                event: 'CONNECTION',
+                username: this.user.username || 'not-logged-in'
+            }))
         }
         this.connection.onmessage = (event) => {
-            console.log('Websocket is getting message', event.data)
-       }
+            let data = {}
+            try {
+                data = JSON.parse(event.data)
+            } catch(err) {
+                console.error(err.message, event.data)
+                return
+            }
+            if(data.event == 'POST') this.posts.push(data)
+        }
     }    
 }
 </script>
   
 <style scoped>
+.toLeft { text-align: left; }
+.toRight { text-align: right; }
 </style>
--- websocket.js
+++ websocket.js
@@ -1,7 +1,14 @@
 module.exports = wsInstance => (ws, req) => {
     ws.on('message', rawData => {
+        let data = {}
+        try {
+            data = JSON.parse(rawData)
+        } catch(err) {
+            console.error(err.message, rawData)
+            return
+        }
         wsInstance.getWss().clients.forEach(client => {
-            client.send(rawData.toUpperCase())
+            client.send(JSON.stringify(data))
         })
     })
 }
